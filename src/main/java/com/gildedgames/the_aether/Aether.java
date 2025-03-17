70f1b1c6e61cc916522c47e932967d86b58907cf
bconlon <bconlon.gml@gmail.com>
2025-03-17 11:23:13 -0700

    bump version

--- src/main/java/com/gildedgames/the_aether/Aether.java
+++ src/main/java/com/gildedgames/the_aether/Aether.java
@@ -38,7 +38,7 @@ public class Aether
 
 	public static final String modid = "aether_legacy";
 
-	public static final String version = "1.5.3.4";
+	public static final String version = "1.5.4.1";
 
 	@Instance(Aether.modid)
 	public static Aether instance;
