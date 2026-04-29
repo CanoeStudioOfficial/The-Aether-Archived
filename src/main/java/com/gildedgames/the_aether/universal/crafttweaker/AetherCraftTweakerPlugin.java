ff7c4d56b3f96cfb2ad82490faef5d30ca27d3d5
Lonelyxiya <3438505487@qq.com>
2026-04-29 19:53:30 +0800

    Update AetherCraftTweakerPlugin.java

--- src/main/java/com/gildedgames/the_aether/universal/crafttweaker/AetherCraftTweakerPlugin.java
+++ src/main/java/com/gildedgames/the_aether/universal/crafttweaker/AetherCraftTweakerPlugin.java
@@ -6,7 +6,6 @@ import crafttweaker.CraftTweakerAPI;
 public class AetherCraftTweakerPlugin {
 
     public static void preInitialization() {
-        CraftTweakerAPI.registerClass(Accessory.class);
         CraftTweakerAPI.registerClass(Enchanter.class);
         CraftTweakerAPI.registerClass(Freezer.class);
     }
